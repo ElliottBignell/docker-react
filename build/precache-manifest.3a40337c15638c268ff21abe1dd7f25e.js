da4b06768efbae44478e7592fe1afc454478d797
ElliottBignell <elliott.bignell@gmail.com>
2018-12-17 09:59:46 +0100

    added travis file

---
+++ build/precache-manifest.3a40337c15638c268ff21abe1dd7f25e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c657a0fc99eb7ada23d8",
+    "url": "/static/js/main.c657a0fc.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "c657a0fc99eb7ada23d8",
+    "url": "/static/css/main.7855e322.chunk.css"
+  },
+  {
+    "revision": "5584b60b122b6ddaf0d649e7e1dc100d",
+    "url": "/index.html"
+  }
+];
